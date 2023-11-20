5720c7eee6ae9cc8051dfc1028beda7a30695464
Karthikkl2001 <karthikkllokesh2001@gmail.com>
2023-11-20 23:28:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def karthik(request):
+    return render(request,'karthik.html')
+
+def ananya(request):
+    return render(request,'ananya.html')
+
+def karthik2(request):
+    return render(request,'karthik2.html')
